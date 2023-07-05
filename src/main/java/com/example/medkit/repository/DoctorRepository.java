64c50c9a8fbe31ec896b6ffe4db5234ad1792bef
Shavkatjon97 <dadshavkatjon97@gmail.com>
2023-07-05 12:05:28 +0500

    Native query changes

--- src/main/java/com/example/medkit/repository/DoctorRepository.java
+++ src/main/java/com/example/medkit/repository/DoctorRepository.java
@@ -12,9 +12,4 @@ import java.util.Optional;
 public interface DoctorRepository extends JpaRepository<DoctorEntity,Long>, CustomRepository {
     Optional<DoctorEntity> findByPhoneNumber(String phoneNumber);
 
-    @Query(value = "select * from md_doctor order by rating desc",nativeQuery = true)
-    List<DoctorEntity> getAllDoctors();
-    @Query(value = "select * from md_doctor md where md.profession like '%:prof%';",nativeQuery = true)
-    List<DoctorEntity> getDoctorByProfession(String prof);
-
 }
