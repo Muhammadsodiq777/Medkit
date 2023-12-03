97b4ebfc9607482d4c3c9d9f886b4830fad4a5c0
csharpdevelope <drakhimov001@gmail.com>
2023-12-03 15:19:26 +0500

    big changes

--- src/main/java/com/example/medkit/MedkitApplication.java
+++ src/main/java/com/example/medkit/MedkitApplication.java
@@ -3,15 +3,11 @@ package com.example.medkit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.example.medkit",
-        "com.example.medkit.config"
-})
+@SpringBootApplication
 public class MedkitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedkitApplication.class, args);
-        System.out.println("Started!!!");
     }
 
 }
